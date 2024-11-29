6bfcd57145bd54a95234886b8c414d8edd8f9d8f
007tickooayush <ayush.tickoo292000@gmail.com>
2024-11-29 20:59:15 +0530

    readdir implementation with Eof condition+create_root_dir also implemented

--- src/session/sftp_sftp_session.rs
+++ src/session/sftp_sftp_session.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::os::unix::prelude::{MetadataExt, PermissionsExt};
 use std::path::PathBuf;
 use std::time::{Duration, UNIX_EPOCH};
 use async_trait::async_trait;
@@ -7,6 +8,7 @@ use log::{error, info};
 use russh_sftp::protocol::{Data, File, FileAttributes, Handle, Name, Status, StatusCode, Version};
 use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
+use crate::utils::utils::create_root_dir;
 
 #[derive(Default)]
 pub struct SftpSession {
@@ -34,12 +36,21 @@ impl russh_sftp::server::Handler for SftpSession {
         }
 
         let root_dir = std::env::var("ROOT_DIR").unwrap_or(".".to_string());
-        self.server_root_dir = PathBuf::from(root_dir);
-        self.cwd = self.server_root_dir.clone();
+
+        match create_root_dir(&root_dir).await {
+            Ok(_) => {
+                // todo("Have different ROOT_DIR for each SFTP client");
+                self.server_root_dir = PathBuf::from(root_dir);
+                self.cwd = self.server_root_dir.clone();
+            },
+            Err(e) => {
+                eprintln!("Error creating root directory: {}", e);
+                return Err(StatusCode::ConnectionLost);
+            }
+        }
 
         self.version = Some(version);
         println!("SftpSession::init: version: {:?} extensions: {:?}", self.version, extensions);
-
 
 
         Ok(Version::new())
@@ -74,28 +85,87 @@ impl russh_sftp::server::Handler for SftpSession {
     async fn readdir(&mut self, id: u32, handle: String) -> Result<Name, Self::Error> {
         // todo!("Handle the readdir function properly");
         println!("SftpSession::readdir: id: {:?} handle: {:?}", id, handle);
-        if handle == "/" && self.root_dir_read_done {
-            self.root_dir_read_done = false;
-            return Ok(Name {
-                id,
-                files: vec![
-                    File::new("foo.txt", FileAttributes::default()),
-                    File::new("bar.txt", FileAttributes::default())
-                ]
-            })
+
+        let path = if handle == "/" {
+            self.server_root_dir.clone()
+        } else {
+            self.server_root_dir.join(handle)
+        };
+
+        let mut contents = tokio::fs::read_dir(path).await.map_err(|_| StatusCode::NoSuchFile)?;
+        let mut files = Vec::new();
+
+        while let Some(entry) = contents.next_entry().await.map_err(|_| StatusCode::Eof)? {
+            let file_name = entry.file_name().into_string().map_err(|_| StatusCode::NoSuchFile)?;
+            let metadata = entry.metadata().await.map_err(|_| StatusCode::Failure)?;
+
+            let file_attrs = FileAttributes {
+                size: Some(metadata.len()),
+                uid: Some(metadata.uid()),
+                gid: Some(metadata.gid()),
+                permissions: Some(metadata.permissions().mode()),
+                atime: Some(metadata.accessed().unwrap_or(UNIX_EPOCH).duration_since(UNIX_EPOCH).unwrap_or(Duration::from_secs(0)).as_secs() as u32),
+                mtime: Some(metadata.modified().unwrap_or(UNIX_EPOCH).duration_since(UNIX_EPOCH).unwrap_or(Duration::from_secs(0)).as_secs() as u32),
+                group: None,
+                user: Some("SFTP-rustified".to_string()),
+            };
+            files.push(File::new(file_name,file_attrs));
         }
 
-        // If all files have been read, return an Err to indicate EOF not empty list
-        Err(StatusCode::Eof)
+        if self.root_dir_read_done {
+            return Err(StatusCode::Eof);
+        }
+
+        self.root_dir_read_done = true;
+
+        Ok(Name {
+            id,
+            files
+        })
+
+        // if handle == "/" && self.root_dir_read_done {
+        //     self.root_dir_read_done = false;
+        //     return Ok(Name {
+        //         id,
+        //         files: vec![
+        //             File::dummy("foo.txt"),
+        //             File::dummy("bar.txt")
+        //         ]
+        //     })
+        // }
+        //
+        // // If all files have been read, return an Err to indicate EOF not empty list
+        // Err(StatusCode::Eof)
     }
 
+    /// Used to Provide the Directory path to be used by the Client like "/"
     async fn realpath(&mut self, id: u32, path: String) -> Result<Name, Self::Error> {
         // todo!("Handle the realpath function properly");
         println!("SftpSession::realpath: id: {:?} path: {:?}", id, path);
+        // let mut used_path ;
+        // if path == "." {
+        //     used_path = self.cwd.clone();
+        // } else {
+        //     used_path = self.cwd.join(path);
+        // }
+        // used_path.push("foo.txt");
+        //
+        // let mut file2 = tokio::fs::File::create(used_path).await.unwrap();
+        // file2.write_all(b"Hello, World!").await.unwrap();
+        //
+        //
+        //
+        // Ok(Name {
+        //     id,
+        //     files: vec![
+        //         File::new("foo.txt", FileAttributes::default()),
+        //     ]
+        // })
+
         Ok(Name {
             id,
             files: vec![
-                File::dummy("/")
+                File::new("/", FileAttributes::default()),
             ]
         })
     }
